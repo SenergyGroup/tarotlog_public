c5239a62af7fd9e3e0e1c29856912f370681eafd
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2024-11-06 20:29:52 -0600

    Switched Neon to Render backend

--- app.js
+++ app.js
@@ -3,23 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 
-// Neon Backend
-const http = require("http");
-const { neon } = require("@neondatabase/serverless");
-
-const sql = neon(process.env.DATABASE_URL);
-const requestHandler = async (req, res) => {
-  const result = await sql`SELECT version()`;
-  const { version } = result[0];
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(version);
-};
-
-http.createServer(requestHandler).listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
-});
-
-
 // Neon Database Backend and API
 const app = express();
 const port = process.env.PORT || 3000;
