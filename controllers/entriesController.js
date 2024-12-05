f2d9ebd36f0297e5a6c6d8d12128f4d6ae0c8bf9
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2024-12-04 18:03:18 -0600

    Oops. Didn't update the config route

--- controllers/entriesController.js
+++ controllers/entriesController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/db'); // PostgreSQL connection pool
+const pool = require('../config/database'); // PostgreSQL connection pool
 
 
 // Fetch entries for logged-in user with optional filters
