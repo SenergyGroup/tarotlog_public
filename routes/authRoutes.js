88ca81549c75e986ac3824c591948b079ac16d34
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2024-12-05 18:53:00 -0600

    logout path update to auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,6 @@ router.post('/login', authConroller.login_post)
 router.get('/tarot', (req, res) => {
     res.render('tarot', { user: res.locals.user }); 
   });
-router.get('/logout', authConroller.logout);
+router.get('/auth/logout', authConroller.logout);
 
 module.exports = router
