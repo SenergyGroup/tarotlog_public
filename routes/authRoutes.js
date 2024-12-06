3d8fcdb75228189b7833064bad4c76df83d1f0bb
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2024-12-05 18:58:45 -0600

    updated logout header link

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,6 @@ router.post('/login', authConroller.login_post)
 router.get('/tarot', (req, res) => {
     res.render('tarot', { user: res.locals.user }); 
   });
-router.get('/auth/logout', authConroller.logout);
+router.get('/logout', authConroller.logout);
 
 module.exports = router
