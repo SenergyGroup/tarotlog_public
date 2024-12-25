165b49d9322e9e753f0a8e13813ae6395928d235
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2024-12-24 21:45:29 -0600

    :3 about to have a meltdown

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const { Router }    = require('express')
-const authConroller = require('../controllers/authController')
+const authController = require('../controllers/authController')
 
 const router = Router()
 
