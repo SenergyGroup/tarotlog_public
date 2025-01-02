7a188b14f0998d79a31a911667193b385d713e4c
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2025-01-01 19:29:26 -0600

    :3

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 let drawnCard = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-    const API_BASE_URL = 'http://localhost:3000';
-    //const API_BASE_URL = 'https://tarotlog-public.onrender.com';
+    //const API_BASE_URL = 'http://localhost:3000';
+    const API_BASE_URL = 'https://tarotlog-public.onrender.com';
 
     const moodSlider = document.getElementById("mood-slider");
     const drawCardBtn = document.getElementById('draw-card-btn');
