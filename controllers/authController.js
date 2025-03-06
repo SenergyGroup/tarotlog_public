4972ca8405319b47599eb7a2d656c981f842fee0
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2025-03-05 20:54:34 -0600

    user already in use error

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,7 +63,11 @@ const signup_post = async (req, res) => {
       });
     }
     if (error.name === 'SequelizeUniqueConstraintError') {
-      errors.email = 'This email is already registered';
+      if (err.path === 'email') {
+        errors.email = 'This email is already registered';
+      } else if (err.path === 'username') {
+        errors.username = 'This username is already taken';
+      }
     }
 
     res.status(400).json({ errors });
